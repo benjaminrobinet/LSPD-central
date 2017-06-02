d0cccfd3cefc560babaccd96b7ad85560a1a49b0
Benjamin Robinet <benjaminrobinet@users.noreply.github.com>
2017-06-02 17:35:51 +0200

    Add list on register

--- pages/register.php
+++ pages/register.php
@@ -59,6 +59,7 @@
     <div class="row">
         <div class="col-xs-12">
             <ul class="nav nav-pills nav-justified">
+                <li role="presentation" class="<?= ($query[0] == "") ? "active" : "" ?>"><a href="<?=$current?>"><?=txt("Liste")?></a></li>
                 <li role="presentation" class="<?= ($query[0] == "add") ? "active" : "" ?>"><a href="<?=$current.'/add'?>"><?=txt("Ajouter")?></a></li>
                 <li role="presentation" class="<?= ($query[0] == "edit") ? "active" : "" ?>"><a href="<?=$current.'/edit'?>"><?=txt("Editer")?></a></li>
             </ul>
